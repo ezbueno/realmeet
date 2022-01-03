0f348f9a1db97f2de8e431463b0afc20327939c7
ezbueno <ezandrobueno@yahoo.com.br>
2022-01-03 17:02:43 -0300

    Criado a validação dos dados de entrada do relatório

--- service/src/main/java/br/com/sw2you/realmeet/report/validator/AbstractReportValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/report/validator/AbstractReportValidator.java
@@ -1,5 +1,22 @@
 package br.com.sw2you.realmeet.report.validator;
 
+import br.com.sw2you.realmeet.report.model.AbstractReportData;
+import br.com.sw2you.realmeet.validator.ValidationErrors;
+
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
+import static br.com.sw2you.realmeet.validator.ValidatorUtils.thrownOnError;
+import static br.com.sw2you.realmeet.validator.ValidatorUtils.validateRequired;
+
 public abstract class AbstractReportValidator {
-    //TODO
+
+    public void validate(AbstractReportData reportData) {
+        var validationErrors = new ValidationErrors();
+
+        validateRequired(reportData.getEmail(), EMAIL, validationErrors);
+        this.validate(reportData, validationErrors);
+
+        thrownOnError(validationErrors);
+    }
+
+    protected abstract void validate(AbstractReportData reportData, ValidationErrors validationErrors);
 }
